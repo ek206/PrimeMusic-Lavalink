eb64fd36a489e04c4c7556888c5c71e243e3682a
ek206 <52988655+ek206@users.noreply.github.com>
2025-03-03 22:53:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["620162365266329621", "706425935364096010"], 
+  mongodbUri : "mongodb+srv://Evil:Evil@music.riy5n.mongodb.net/?retryWrites=true&w=majority&appName=music",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/codescript",
   embedTimeout: 5, 
-  errorLog: "", 
+  errorLog: "",  
   nodes: [
      {
       name: "XX",
